798f23cf7cb72c311d22da497e58afea92bb7b23
Srimathi1306 <srimathi600201@gmail.com>
2025-07-22 23:31:23 +0530

    added index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import App from "./App";
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
