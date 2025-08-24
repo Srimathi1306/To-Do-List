4ac97677a808908f7d89bc7881e2b8982af678fb
Srimathi1306 <srimathi600201@gmail.com>
2025-08-24 23:21:27 +0530

    updated css

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,10 @@
 export default function List() {
   return (
     <form className="m-8 flex gap-4">
-      <input type="text" placeholder="Add to list" />
-      <button type="submit">Add List</button>
+      <input type="text" placeholder="Add to list" className="border" />
+      <button type="submit" classNmae="">
+        Add List
+      </button>
     </form>
   );
 }
