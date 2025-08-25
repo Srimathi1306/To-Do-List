f0c6b0cda7c32b5f7baaff0ca9ca7150cc3b6765
Srimathi1306 <srimathi600201@gmail.com>
2025-08-25 22:43:38 +0530

    css update in button

--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,21 @@
 export default function List() {
   return (
-    <form className="m-8 flex gap-4">
-      <input type="text" placeholder="Add to list" className="border" />
-      <button type="submit" classNmae="">
-        Add List
-      </button>
-    </form>
+    <div>
+      <form className="m-8 flex gap-4">
+        <input type="text" placeholder="Add to list" className="border" />
+        <button type="submit" classNmae="bg-blue-500 ">
+          Add List
+        </button>
+      </form>
+      <div
+        className="bg-blue-300 text-white px-4 py-2 rounded-lg shadow-md
+          transition-transform duration-200 ease-in-out
+          hover:bg-blue-600 hover:scale-105 hover:shadow-lg
+          active:scale-95 active:bg-blue-700
+          focus:ring-2 focus:ring-blue-300"
+      >
+        hello
+      </div>
+    </div>
   );
 }
