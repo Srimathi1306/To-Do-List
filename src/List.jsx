bcd235a11c51bc38a7fad6dc2fe6888d74abf77f
Srimathi1306 <srimathi600201@gmail.com>
2025-08-28 21:13:40 +0530

    added state

--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 export default function List() {
+  const [inputValue, setInputValue] = useState(""); // state to store input
+  const [list, setList] = useState([]); // state to store all items
   return (
     <div>
       <form className="m-8 flex gap-4">
@@ -18,15 +21,6 @@ export default function List() {
           Add List
         </button>
       </form>
-      <button
-        className="bg-blue-300 text-white px-4 py-2 rounded-lg shadow-md
-          transition-transform duration-200 ease-in-out
-          hover:bg-blue-600 hover:scale-105 hover:shadow-lg
-          active:scale-95 active:bg-blue-700
-          focus:ring-2 focus:ring-blue-300"
-      >
-        hello
-      </button>
     </div>
   );
 }
